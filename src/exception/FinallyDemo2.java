2f8281d1428cfdd1be70759cf898e3eba61f01ca
Xiloer <83123095@qq.com>
2022-02-08 14:38:52 +0800

    day06

---
+++ src/exception/FinallyDemo2.java
@@ -0,0 +1,31 @@
+package exception;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 异常处理机制在IO操作中的应用
+ */
+public class FinallyDemo2 {
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("fos.dat");
+            fos.write(1);
+        }catch(IOException e){
+            System.out.println("出错了！在这里解决了!");
+        }finally{
+            try {
+                if(fos!=null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+
