5428fdc17683ad8acc21035cad1767259da34d18
Xiloer <83123095@qq.com>
2022-02-08 11:49:40 +0800

    day06

--- src/exception/TryCatchDemo.java
+++ src/exception/TryCatchDemo.java
@@ -16,19 +16,32 @@ public class TryCatchDemo {
         System.out.println("程序开始了");
         try {
 //            String str = null;
-            String str = "";
+//            String str = "";
+            String str = "a";
             //这里会出现空指针异常，虚拟机会实例化空指针异常实例并在这里抛出
             System.out.println(str.length());
             System.out.println(str.charAt(0));
+            System.out.println(Integer.parseInt(str));
+            //try语句块中某句话报错后，那么try语句块中这句话之后的代码均不会执行!
+            System.out.println("!!!!!!!!!!!!!!!!");
 
-        }catch(NullPointerException e){
-            System.out.println("出现了空指针，并在这里得以解决!");
-        //catch可以定义多个，针对不同的异常有不同处理办法时，可以分别捕获并进行处理
-        }catch(StringIndexOutOfBoundsException e){
-            System.out.println("出现了字符串下标越界了！并在这里解决!");
+          //批量注释:选中需要注释的代码片段按"ctrl+/"
+//        }catch(NullPointerException e){
+//            System.out.println("出现了空指针，并在这里得以解决!");
+//        //catch可以定义多个，针对不同的异常有不同处理办法时，可以分别捕获并进行处理
+//        }catch(StringIndexOutOfBoundsException e){
+//            System.out.println("出现了字符串下标越界了！并在这里解决!");
+//        }
+        //可以合并捕获异常，当不同异常处理手段相同时，可以用这种方式。
+        }catch(NullPointerException|StringIndexOutOfBoundsException e){
+            System.out.println("出现了空指针或下标越界的处理!");
+
+        }catch(Exception e){
+            System.out.println("反正就是出了个错!");
         }
         System.out.println("程序结束了");
     }
+
 }
 
 
