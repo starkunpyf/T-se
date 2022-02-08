0fce8e624f39763cb52b5c719d7cde2015d633ac
Xiloer <83123095@qq.com>
2022-02-08 10:42:49 +0800

    day06

--- src/exception/TryCatchDemo.java
+++ src/exception/TryCatchDemo.java
@@ -8,6 +8,20 @@ package exception;
  * }catch(XXXException e){
  *     当try中出现XXXException后的解决办法
  * }
+ *
+ * try语句块不能独立存在，后面必须跟catch或finally。
  */
 public class TryCatchDemo {
+    public static void main(String[] args) {
+        System.out.println("程序开始了");
+
+        String str = null;
+        System.out.println(str.length());
+
+        System.out.println("程序结束了");
+    }
 }
+
+
+
+
