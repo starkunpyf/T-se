154c7d5b65b09b3da5a8f51294d0aeb613dfaf99
Xiloer <83123095@qq.com>
2022-02-08 10:35:10 +0800

    day06

---
+++ src/exception/TryCatchDemo.java
@@ -0,0 +1,13 @@
+package exception;
+
+/**
+ * JAVA异常处理机制中的try-catch
+ * 语法:
+ * try{
+ *     可能出现异常的代码片段
+ * }catch(XXXException e){
+ *     当try中出现XXXException后的解决办法
+ * }
+ */
+public class TryCatchDemo {
+}
