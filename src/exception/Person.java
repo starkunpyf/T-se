a565266a786e2c2f1fc435740c85cbbe92d56c2c
Xiloer <83123095@qq.com>
2022-02-08 16:12:52 +0800

    day06

--- src/exception/Person.java
+++ src/exception/Person.java
@@ -10,12 +10,12 @@ public class Person {
         return age;
     }
 
-    public void setAge(int age){
+    public void setAge(int age)throws Exception{
         if(age<0||age>100){
 //            throw new RuntimeException("年龄不合法!");
             /*
                 java中除了RuntimeException之外的其他异常throw抛出是编译器要求
-                必须在方法上使用throws声明该异常的抛出通知调用者处理该异常。
+                必须在方法上使用throws声明该异常的抛出
              */
             throw new Exception("年龄不合法!");
         }
