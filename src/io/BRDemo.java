a10cf3b5ff88288d05cd11f0575d02ece9b4736d
Xiloer <83123095@qq.com>
2022-02-08 09:41:29 +0800

    day06

--- src/io/BRDemo.java
+++ src/io/BRDemo.java
@@ -16,7 +16,16 @@ public class BRDemo {
         InputStreamReader isr = new InputStreamReader(fis);
         //缓冲字符输入流，高级流，字符流。功能:1块读文本数据加速 2:按行读取字符串
         BufferedReader br = new BufferedReader(isr);
+        /*
+            缓冲字符输入流提供的读取一行字符串的方法:
+            String readLine()
+            该方法会连续读取若干字符，到换行符为止，然后将换行符之前的内容以一个字符串形式
+            返回。注意:返回的字符串中不含有最后的换行符。
+            如果单独读取了空行(此行内容只有一个换行符，比如当前源代码中的第二行就是空行)，
+            那么会返回一个空字符串。
+            当方法返回值为null时，表示流读取到了末尾。
 
+         */
         String line = br.readLine();
         System.out.println(line);
 
