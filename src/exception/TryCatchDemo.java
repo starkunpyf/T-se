96d67dda415cded3bd7eb18d2a7e27ac5a67d030
Xiloer <83123095@qq.com>
2022-02-08 11:28:20 +0800

    day06

--- src/exception/TryCatchDemo.java
+++ src/exception/TryCatchDemo.java
@@ -14,10 +14,19 @@ package exception;
 public class TryCatchDemo {
     public static void main(String[] args) {
         System.out.println("程序开始了");
+        try {
+//            String str = null;
+            String str = "";
+            //这里会出现空指针异常，虚拟机会实例化空指针异常实例并在这里抛出
+            System.out.println(str.length());
+            System.out.println(str.charAt(0));
 
-        String str = null;
-        System.out.println(str.length());
-
+        }catch(NullPointerException e){
+            System.out.println("出现了空指针，并在这里得以解决!");
+        //catch可以定义多个，针对不同的异常有不同处理办法时，可以分别捕获并进行处理
+        }catch(StringIndexOutOfBoundsException e){
+            System.out.println("出现了字符串下标越界了！并在这里解决!");
+        }
         System.out.println("程序结束了");
     }
 }
