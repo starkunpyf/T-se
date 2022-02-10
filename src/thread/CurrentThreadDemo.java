739daf23fa812703600ee4db1039020faa61c0f0
Xiloer <83123095@qq.com>
2022-02-10 14:14:55 +0800

    day09

---
+++ src/thread/CurrentThreadDemo.java
@@ -0,0 +1,16 @@
+package thread;
+
+/**
+ * java中所有的代码都是靠线程运行的，main方法也不例外。JVM启动后会创建一个线程来执行
+ * main方法，并且该线程的名字也叫"main",因此我们通常称它为"主线程"。但是它和我们创建
+ * 的线程并无区别。
+ *
+ * Thread提供了一个静态方法:
+ * static Thread currentThread()
+ * 该方法可以获取运行这个方法的线程。
+ *
+ * current:当前的
+ *
+ */
+public class CurrentThreadDemo {
+}
