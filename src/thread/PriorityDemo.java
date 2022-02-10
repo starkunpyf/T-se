f24e237e34ac41c7eae6964b8c739b5c8426eaa9
Xiloer <83123095@qq.com>
2022-02-10 16:55:03 +0800

    day09

--- src/thread/PriorityDemo.java
+++ src/thread/PriorityDemo.java
@@ -12,6 +12,32 @@ package thread;
  */
 public class PriorityDemo {
     public static void main(String[] args) {
-
+        Thread min = new Thread(){
+            public void run(){
+                for (int i=0;i<10000;i++){
+                    System.out.println("min");
+                }
+            }
+        };
+        Thread norm = new Thread(){
+            public void run(){
+                for (int i=0;i<10000;i++){
+                    System.out.println("nor");
+                }
+            }
+        };
+        Thread max = new Thread(){
+            public void run(){
+                for (int i=0;i<10000;i++){
+                    System.out.println("max");
+                }
+            }
+        };
+//        min.setPriority(1);
+        min.setPriority(Thread.MIN_PRIORITY);
+        max.setPriority(Thread.MAX_PRIORITY);
+        min.start();
+        norm.start();
+        max.start();
     }
 }
