782a228f5c6beacb8b952484ea1ffd3a0e6c631e
Xiloer <83123095@qq.com>
2022-02-11 14:41:12 +0800

    day10

---
+++ src/thread/SyncDemo1.java
@@ -0,0 +1,52 @@
+package thread;
+
+/**
+ * 多线程并发安全问题
+ * 当多个线程并发操作同一临界资源，由于线程切换实际不确定，导致操作顺序出现混乱而引起
+ * 各种逻辑错误。
+ * 临界资源:操作该资源的完整过程同一时刻只能由单线程进行。
+ */
+public class SyncDemo1 {
+    public static void main(String[] args) {
+        Table table = new Table();
+        Thread t1 = new Thread(){
+            public void run(){
+                while(true){
+                    int bean = table.getBean();
+                    Thread.yield();//让出
+                    System.out.println(getName()+":"+bean);
+                }
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                while(true){
+                    int bean = table.getBean();
+                    Thread.yield();//让出
+                    System.out.println(getName()+":"+bean);
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+}
+
+class Table{
+    private int beans = 20;//桌子上有20个豆子
+
+    public int getBean(){
+        if(beans==0){//桌子上没有豆子了!
+            throw new RuntimeException("没有豆子了!");
+        }
+        Thread.yield();//让出
+        return beans--;
+    }
+}
+
+
+
+
+
+
+
