e525b3db11f3d3d401ad7c433548104574bba5c5
Xiloer <83123095@qq.com>
2022-02-14 16:12:43 +0800

    day10

---
+++ src/collection/CollectionDemo1.java
@@ -0,0 +1,17 @@
+package collection;
+
+/**
+ * JAVA集合框架
+ * 集合和数组一样，可以保存一组元素，但是集合将元素的操作都封装成了方法，操作简便。
+ * 并且集合提供了多种不同的实现供我们使用。
+ *
+ * java.util.Collection是所有集合的顶级接口，里面定义了所有集合都必须具备的功能方法
+ * 集合有两类常用的子类:
+ * java.util.List:可重复的集合，且有序。通常我们称它为"线性表"
+ * java.util.Set:不可重复的集合。
+ * 上述两个都是接口，而元素是否重复拒绝于元素自身的equals方法，即:Set集合中不会存在
+ * 两个元素equals比较为true的情况。
+ *
+ */
+public class CollectionDemo1 {
+}
