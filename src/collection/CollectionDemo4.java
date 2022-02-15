c357b560a7cac32df77d379a1c71ee695765921e
Xiloer <83123095@qq.com>
2022-02-15 09:42:22 +0800

    day10

--- src/collection/CollectionDemo4.java
+++ src/collection/CollectionDemo4.java
@@ -28,6 +28,33 @@ public class CollectionDemo4 {
         System.out.println("c1:"+c1);
         System.out.println("c2:"+c2);
 
+        Collection c3 = new ArrayList();
+        c3.add("c++");
+        c3.add("ios");
+        c3.add("php");
+        System.out.println("c3:"+c3);
+        /*
+            boolean containsAll(Collection c)
+            判断当前集合是否包含给定集合中的所有元素
+         */
+        boolean containsAll = c1.containsAll(c3);
+        System.out.println("包含所有:"+containsAll);
+
+        /*
+            取交集
+         */
+        c1.retainAll(c3);
+        System.out.println("c1:"+c1);
+        System.out.println("c3:"+c3);
+
+        /*
+            删除交集。将c1中与c3的共有元素删除(c3不受影响)
+         */
+        c1.removeAll(c3);
+        System.out.println("c1:"+c1);
+        System.out.println("c3:"+c3);
+
+
     }
 }
 
