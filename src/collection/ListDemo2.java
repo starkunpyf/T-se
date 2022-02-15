f127a9aa6f45ec08d4bd1c0c13ba5eb609b41eb2
Xiloer <83123095@qq.com>
2022-02-15 15:48:58 +0800

    day10

--- src/collection/ListDemo2.java
+++ src/collection/ListDemo2.java
@@ -17,7 +17,22 @@ public class ListDemo2 {
         list.add("five");
         System.out.println(list);
 
+        /*
+            void add(int index,E e)
+            将给定元素插入到指定位置
+         */
+        //[one,two,three,six,four,five]
+        list.add(3,"six");
+        System.out.println(list);
 
+        /*
+            E remove(int index)
+            删除并返回指定位置上的元素
+         */
+        //[one,two,six,four,five]
+        String old = list.remove(2);
+        System.out.println(list);
+        System.out.println(old);
     }
 }
 
