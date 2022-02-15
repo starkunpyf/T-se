829710d4902738b79231b4faeeb4286b98174843
Xiloer <83123095@qq.com>
2022-02-15 14:11:58 +0800

    day10

---
+++ src/collection/ListDemo.java
@@ -0,0 +1,13 @@
+package collection;
+
+/**
+ * java.util.List接口
+ * List继承自Collection，是最常用的一类集合。特点:可以存放重复元素且有序。
+ * List里提供了一套可以通过下标操作元素的方法。
+ * 常用实现类:
+ * java.util.ArrayList:内部使用数组实现，查询性能更好。
+ * java.util.LinkedList:内部使用链表实现，增删性能更好，首尾增删性能最佳。
+ * 对性能没有特别苛刻的要求下，通常使用ArrayList即可。
+ */
+public class ListDemo {
+}
